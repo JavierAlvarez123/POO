93ca6b81e24f6d32bd7923e87552644a3107c7fd
JavierAlvarez123 <j.alvarez230870@gmail.com>
2025-04-15 14:05:41 -0400

    actividad1

--- Guías (POO)/Guía N°1.py
+++ Guías (POO)/Guía N°1.py
@@ -1,17 +1,17 @@
 # Ejercicio1
 
-"""edad= 18
+edad= 18
 altura= 1.82
 nombre= "Javier"
 es_estudiante= True
 print(type(edad))
 print(type(altura))
 print(type(nombre))
-print(type(es_estudiante))"""
+print(type(es_estudiante))
 
 # Ejercicio2
 
-"""dato1=int(input("ingresa un numero: "))
+dato1=int(input("ingresa un numero: "))
 dato2=int(input("ahora ingresa otro numero para sumarlos: "))
 suma= dato1+dato2
 print("Este es el resultado: ",suma)
@@ -31,11 +31,11 @@ B=input("Ingresa el segundo numero: ")
 if A>B:
     print(A," Es mayor que ",B)
 else:
-    print(B," Es mayor que ",A)"""
+    print(B," Es mayor que ",A)
 
 # Ejercicio3
 
-"""Q=int(input("ingresa un número entero para convertirlo en flotante: "))
+Q=int(input("ingresa un número entero para convertirlo en flotante: "))
 W=float(Q)
 print("Este es el número: ",W)
 
@@ -49,11 +49,27 @@ print("Este es el número convertido: ",Y)
 
 U=str(input("ingresa una cadena numerica para convertirla a un número: "))
 I=int(U)
-print("Este es el texto  convertido a número: ",I)"""
+print("Este es el texto  convertido a número: ",I)
 
 # Ejercicio4
 
 nombre=input("Ingresa tu nombre: ")
-edad=input("Ingresa tu edad: ")
-years_10= edad
+edad=int(input("Ingresa tu edad: "))
+years_10= edad+10
 print("tu edad en diez años sera de: ",years_10)
+
+# Desafio final
+
+K=float(input("Ingresa un numero decimal: "))
+L=float(input("Ingresa un segundo numero decimal: "))
+O=int(K)
+P=int(L)
+suma=O+P
+resta=O-P
+multiplicacion=O*P
+division=O/P
+print("Los numeros ",K,"y",L,"se han convertido a: ",O,"y",P)
+print("Esta es la suma de ambos numeros: ",suma)
+print("Esta es la resta de ambos numeros: ",resta)
+print("Esta es la multiplicación de ambos numeros: ",multiplicacion)
+print("Esta es la division de ambos numeros: ",division)
