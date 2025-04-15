e2f2ffdb71613029d4e34334efc30756cd5636c5
JavierAlvarez123 <j.alvarez230870@gmail.com>
2025-04-15 14:34:57 -0400

    hola

--- Actividad1/frutas.py
+++ Actividad1/frutas.py
@@ -4,17 +4,16 @@ avocado = int(input("ingrese la cantidad avocado:" ))
 apple = int(input("ingrese la cantidad apple:" ))
 
 total_frutas = watermelon + cucumber + avocado + apple
-print(total_frutas) 
+print("Este es  el total de frutas: ",total_frutas) 
 total_rojas = watermelon+apple 
 print(total_rojas)
 total_verdes = avocado+cucumber
 
 total_rojas =  watermelon+apple
-print("el grupo de las frutas rojas es de" +total_rojas+ "y el grupo de las frutas verdes es de"+total_verdes)
-
+print("el grupo de las frutas rojas es de " +str(total_rojas)+ " y el grupo de las frutas verdes es de "+str(total_verdes))
 if total_verdes > total_rojas:
     print("las frutas verdes es el grupo mas grande.")
 elif total_verdes < total_rojas:
     print("las frutas rojas es el grupo mas grande.")
 else :
-    print("contiene la misma cantidad de frutas.")
+    print("contienen la misma cantidad de frutas.")
