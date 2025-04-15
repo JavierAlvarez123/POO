6ee76a507d392bc9550124299770cf279f10d801
JavierAlvarez123 <j.alvarez230870@gmail.com>
2025-04-15 17:21:16 -0400

    actividad2

---
+++ Listas/Listas.py
@@ -0,0 +1,17 @@
+lista=["manzana","limon","naranja","naranja"]
+print(lista)
+lista2=[1,2,3]
+lista3=["manzana",3,True]
+
+listaverduras= list("tomates","betarraga")
+
+
+#Se aplica el slicing a las listas
+#print(lista[1])
+#print(lista[1:])
+#print(lista[0:1])
+#print(lista[-1:])
+
+#contiene otros tipos de datos
+#Se almacenan los numeros de manera ordenada
+#mutable
---
+++ Listas/actividad2.py
@@ -0,0 +1,28 @@
+# Ejercicio2
+
+lista=["hola",7,"aloh",True,8]
+print(lista)
+print(lista[0])
+print(lista[4])
+
+print(len(lista))
+
+# Ejercicio2
+
+lista2=["Yajirobe","trunks","vegetta","Goku","Gohan"]
+x=0
+while x < len(lista2):
+    print(lista2[x])
+    x+=1
+print(lista2)
+
+# Ejercicio3
+
+lista2.remove("Goku")
+lista2.insert(3,"Milk")
+print(lista2)
+
+# Ejercicio4
+
+lista2.extend(lista)
+print(lista2)
